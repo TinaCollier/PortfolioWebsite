cd1c3520098217a0703c9fd8f6b234a02a98227d
Tina Collier <tinacollier@Tinas-MacBook-Pro.local>
2022-06-12 19:03:49 -0400

    initial commit

---
+++ portfolio.js
@@ -0,0 +1,32 @@
+// This is the JS for Tina's Portfolio Website
+
+
+
+const project1 = document.getElementById("project1");
+
+project1.addEventListener("click", () => {
+    project1.style.visibility = "hidden";
+    setTimeout(function(){
+        project1.style.visibility = "visible";
+    }, 3000);
+})
+
+
+
+const project2 = document.getElementById("project2");
+
+project2.addEventListener("click", () => {
+    project2.style.visibility = "hidden";
+    setTimeout(function(){
+        project2.style.visibility = "visible";
+    }, 3000);
+})
+
+const project3 = document.getElementById("project3");
+
+project3.addEventListener("click", () => {
+    project3.style.visibility = "hidden";
+    setTimeout(function(){
+        project3.style.visibility = "visible";
+    }, 3000);
+})
